5fe3844a64c47ee2c2b17b3c120d565f938fc935
Arthur Asatryan <biacoder@gmail.com>
2017-12-14 15:19:14 +0400

    Apply #23 changes.

--- src/main/kotlin/org/biacode/jleadboxer/client/JLeadBoxerClient.kt
+++ src/main/kotlin/org/biacode/jleadboxer/client/JLeadBoxerClient.kt
@@ -1,11 +1,15 @@
 package org.biacode.jleadboxer.client
 
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelManager
 import org.biacode.jleadboxer.client.dataset.impl.DatasetResourceClientImpl
 import org.biacode.jleadboxer.client.helper.ResourceClientHelper.readWriteLazy
 import org.biacode.jleadboxer.client.leadboard.impl.LeadBoardResourceClientImpl
 import org.biacode.jleadboxer.client.smartlist.impl.SmartListResourceClientImpl
 import org.biacode.jleadboxer.client.user.impl.UserResourceClientImpl
 import org.biacode.jleadboxer.client.userdataset.impl.UserDatasetResourceClientImpl
+import javax.ws.rs.core.HttpHeaders
+import javax.ws.rs.core.MediaType
 
 /**
  * Created by Arthur Asatryan.
@@ -22,4 +26,10 @@ object JLeadBoxerClient {
     val user by readWriteLazy { UserResourceClientImpl() }
 
     val userDataset by readWriteLazy { UserDatasetResourceClientImpl() }
+
+    init {
+        Fuel.regularMode()
+        FuelManager.instance.basePath = "https://kibana.leadboxer.com/api"
+        FuelManager.instance.baseHeaders = mapOf(HttpHeaders.CONTENT_TYPE to MediaType.APPLICATION_JSON)
+    }
 }
